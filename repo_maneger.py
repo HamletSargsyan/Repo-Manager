1680e4a2d05cdec3a3c243d925cb838d7150d2f1
Hamlet <hamlets849@gmail.com>
2023-12-11 22:56:32 +0400

    Create repo_maneger.py

---
+++ repo_maneger.py
@@ -0,0 +1,18 @@
+import requests
+
+class RepoManeger:
+    def __init__(self) -> None:
+        pass
+
+    def install(self):
+        ...
+    
+    def update(self):
+        ...
+    
+    def uninstall(self):
+        ...
+
+    def list(self):
+        ...
+
