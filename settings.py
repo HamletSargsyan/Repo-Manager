41ae401ff31db4c5b24cc962d304960d1ce49fc8
Hamlet <hamlets849@gmail.com>
2023-12-11 22:56:23 +0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,33 @@
+from typing import List, Union, Tuple, Any
+import json
+
+from alert import alert
+
+class Settings:
+    def __init__(self) -> None:
+        pass
+    
+    def open(self, path: str = 'settings.json') -> Union[dict, None]:
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+                return dict(json.load(f))
+        except json.JSONDecodeError:
+            alert('Произошла ошибка при загрузке настроек')
+
+    def save(self, data: dict, path: str = 'settings.json') -> None:
+        try:
+            with open(path, "w", encoding="utf-8") as f:
+                json.dump(data, f)
+        except json.JSONDecodeError:
+            alert('Произошла ошибка при сохранении настроек')
+
+    def get(self, key: str) -> Union[Any, None]:
+        return self.open().get(key)
+
+    def get_categories(self) -> List[Tuple]:
+        categories = []
+        file = self.open()
+        for index, category in enumerate(file, start=1):
+            if category not in categories:
+                categories.append((category, index))
+        return categories
